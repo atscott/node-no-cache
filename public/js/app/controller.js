4bfa66f0b08049fac36cabd0bb22aa055ad5f702
AScott <atscott01@gmail.com>
2014-08-13 10:45:56 -0500

    changed a formatting

--- public/js/app/controller.js
+++ public/js/app/controller.js
@@ -1,6 +1,5 @@
-angular.module('app')
-    .controller('MessageCtrl', ['$scope',
-        function ($scope) {
-			$scope.message = "It Works!"
-		}
-	]);
+angular.module('app').controller('MessageCtrl', ['$scope',
+    function ($scope) {
+        $scope.message = "It Works!"
+    }
+]);
